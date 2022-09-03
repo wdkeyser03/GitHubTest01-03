15235158fee3ca230b7380f8b9becfb29aea0872
willydekeyser <willy.de.keyser@skynet.be>
2022-09-03 15:48:36 +0200

    Setup GitHub Test 01-03

---
+++ GitHubTest01-03/src/test/java/willydekeyser/GitHubTest0103ApplicationTests.java
@@ -0,0 +1,13 @@
+package willydekeyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubTest0103ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
